8fe2baa0509f063bd7d180aac355c41ded44a327
DMBiryukow <your_github_email>
2024-11-09 18:16:36 +0300

    test_f5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+SECRET_KEY = os.getenv('SECRET_KEY',default='123')
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.168.0.0.1', '84.201.140.137', 'ololoshka2009.serveminecraft.net']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
